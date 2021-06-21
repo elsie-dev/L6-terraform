958ca5889ea40b21bebf3ad343f8fedff4f26bcb
elsie-dev <elsiemarion@gmail.com>
2021-06-21 19:10:59 +0300

    modified my file to deploy 20 instances

--- project.tf
+++ project.tf
@@ -1,10 +1,20 @@
 #iam credentials
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAWASELB6UOXBTPALX"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""#security vulnerabilitiess
 }
-resource "aws_instance" "first-instance" {
-  ami           = "ami-0d8d212151031f51c "
+
+resource "aws_instance" "instance-1"{
+  ami = "ami-0aeeebd8d2ab47354"
   instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  count         = var.instance_count
+  tags = {
+      Name  = "Terraform-${count.index + 1}"
+  }
+  
+}
+variable "instance_count" {
+  default = "20"
 }

307e3c66e3039e0f784562c800f50b2a1d8e6bed
elsie-dev <elsiemarion@gmail.com>
2021-06-22 14:24:09 +0300

    updating the loadbalancer

--- project.tf
+++ project.tf
@@ -1,15 +1,15 @@
 #iam credentials
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAWASELB6UL2GGBMFI"
-  secret_key = "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" #security vulnerabilitiess
+  access_key = ""
+  secret_key =""
 }
 
 resource "aws_instance" "instance"{
   ami = "ami-0aeeebd8d2ab47354"
   instance_type = "t2.micro"
   availability_zone = "us-east-1a"
-  count         = var.instance_count
+  count         = 20 # var.instance_count
   tags = {
       Name  = "Terraform-${count.index + 1}"
   }
@@ -64,3 +64,70 @@ resource "aws_autoscaling_group" "instance" {
     propagate_at_launch = true
   }
 }
+#CREATING AN ELASTIC LOADBALANCER
+# resource "aws_elb" "server" {
+#   name               = "terraform-elb"
+#   availability_zones = data.aws_availability_zones.all.names
+
+#   access_logs {
+#     bucket        = "foo"
+#     bucket_prefix = "bar"
+#     interval      = 60
+#   }
+#   listener {
+#     lb_port                          = 80
+#     lb_protocol                      = "http"
+#     instance_port                    = var.server_port
+#     instance_instance_protocol       = "http"
+#   }
+#   health_check {
+    
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#     timeout             = 3
+#     target              = "HTTP:${var.server_port}/"
+#     interval            = 30
+#   }
+
+#   cross_zone_load_balancing   = true
+#   idle_timeout                = 400
+#   connection_draining         = true
+#   connection_draining_timeout = 400
+
+#   tags = {
+#     Name = "Load-balancer Terraform"
+#   }
+# }
+
+
+resource "aws_elb" "example" {
+  name               = "terraform-asg-example"
+  security_groups    = [aws_security_group.instance.id]
+  availability_zones = data.aws_availability_zones.all.names
+
+   health_check {
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+  
+  # This adds a listener for incoming HTTP requests.
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = var.server_port
+    instance_protocol = "http"
+  }
+}
+#adding ssl certificate course of constant errors
+# resource "aws_acm_certificate" "default" {
+#   provider = "aws.acm"
+#   domain_name = "${var.domain}"
+#   subject_alternative_names = ["*.${var.domain}"]
+#   validation_method = "DNS"
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
